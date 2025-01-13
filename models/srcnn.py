866d2d46007238609437a540ffce09bab3942cc0
ania <45045898+anmaran@users.noreply.github.com>
2025-01-13 22:06:10 +0100

    add: residual layer to srcnn

--- models/srcnn.py
+++ models/srcnn.py
@@ -4,13 +4,14 @@ from torch import nn
 
 
 class SRCNN(nn.Module):
-    def __init__(self, num_channels=1, scale_factor=2):
+    def __init__(self, num_channels=1, scale_factor=2, n_feats = 128):
         super().__init__()
         self.scale_factor = scale_factor
         self.num_channels = num_channels
-        self.conv1 = nn.Conv2d(num_channels, 128, kernel_size=9, padding=9 // 2)
-        self.conv2 = nn.Conv2d(128, 64, kernel_size=5, padding=5 // 2)
-        self.conv3 = nn.Conv2d(64, num_channels, kernel_size=5, padding=5 // 2)
+        self.n_feats = n_feats
+        self.conv1 = nn.Conv2d(num_channels, n_feats, kernel_size=9, padding=9 // 2)
+        self.conv2 = nn.Conv2d(n_feats, n_feats//2, kernel_size=5, padding=5 // 2)
+        self.conv3 = nn.Conv2d(n_feats//2, num_channels, kernel_size=5, padding=5 // 2)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
@@ -23,7 +24,10 @@ class SRCNN(nn.Module):
             recompute_scale_factor=None,
             antialias=False,
         )
+        identity = x                # do residual
         x = self.relu(self.conv1(x))
         x = self.relu(self.conv2(x))
         x = self.conv3(x)
+        x+=identity                 # do residual
+        x = self.relu(x)            # do residual
         return x
